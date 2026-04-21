ba543db9c08aefb463bf2c53dc6be35c0576b212
ryelin715 <26ytngr099c@student.ri.edu.sg>
2026-04-21 15:48:18 +0800

    Update web_app_project_flask.py

--- web_app_project_flask.py
+++ web_app_project_flask.py
@@ -1,9 +1,14 @@
 import sqlite3
-def insert_task(title, desc, assignedmember, createdby, priority="Low", status="In Progress"):
+def insert_task(title, desc, assignedmember, createdby, status="In Progress", priority="Low"):
+  connection = sqlite3.connect("tasks.db")
   connection.execute('''INSERT INTO Tasks(Title, Description, AssignedMember, CreatedBy, Priority)
 VALUES(?,?,?,?,?)''', (title, desc, assignedmember, createdby, priority, status))
+  connection.close()
   return True
 
-def filter_task(, )
+def filter_task(filterby, value):
+  connection = sqlite3.connect("tasks.db")
+  cursor = connection.execute('''SELECT * FROM Tasks WHERE ? = ?''', (filterby, value))
+  connection.close()
 
-connection.close()
+  
