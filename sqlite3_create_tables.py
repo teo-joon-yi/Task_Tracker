6314aabb984535414ae8d29a004494417647aac3
ryelin715 <26ytngr099c@student.ri.edu.sg>
2026-04-21 22:43:08 +0800

    Update sqlite3_create_tables.py

--- sqlite3_create_tables.py
+++ sqlite3_create_tables.py
@@ -4,29 +4,29 @@ connection = sqlite3.connect("tasks.db") #connects to task database
 
 #creates task table
 connection.execute('''CREATE TABLE IF NOT EXISTS Tasks(
-TaskID INTEGER PRIMARY KEY AUTOINCREMENT,
 Title TEXT,
 Description TEXT,
 AssignedMember TEXT,
 CreatedBy TEXT,
 Priority TEXT,
 Status TEXT
+
+PRIMARY KEY(Title)
 )''')
 
 #create people table
 connection.execute('''CREATE TABLE IF NOT EXISTS People(
-PeopleID INTEGER PRIMARY KEY AUTOINCREMENT,
 Name TEXT
 )''') #if we create a login feature can add password or smth
 
 #create project table
 connection.execute('''CREATE TABLE IF NOT EXISTS Project(
 ProjectID INTEGER PRIMARY KEY AUTOINCREMENT,
-TaskID INTEGER,
-PeopleID INTEGER,
+Title INTEGER,
+Name INTEGER,
 
-FOREIGN KEY(TaskID) REFERENCES Tasks(TaskID),
-FOREIGN KEY(PeopleID) REFERENCES People(PeopleID)
+FOREIGN KEY(Title) REFERENCES Tasks(Title),
+FOREIGN KEY(Name) REFERENCES People(Name)
 )''')
 
 connection.close()
