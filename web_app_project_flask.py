73e3b51ab3d50dbd9729b97cbfa2e681b8c68384
ryelin715 <26ytngr099c@student.ri.edu.sg>
2026-04-21 13:35:45 +0800

    Update web_app_project_flask.py

--- web_app_project_flask.py
+++ web_app_project_flask.py
@@ -1,1 +1,9 @@
 import sqlite3
+def insert_task(title, desc, assignedmember, createdby, priority="Low", status="In Progress"):
+  connection.execute('''INSERT INTO Tasks(Title, Description, AssignedMember, CreatedBy, Priority)
+VALUES(?,?,?,?,?)''', (title, desc, assignedmember, createdby, priority, status))
+  return True
+
+def filter_task(, )
+
+connection.close()
